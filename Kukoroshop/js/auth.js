ec08a9a39dfe5969d07eb0bb96c07fc893572cdd
Wittiry443 <ecanasj@unicartagena.edu.co>
2025-12-07 21:15:11 -0500

    Update auth.js

--- Kukoroshop/js/auth.js
+++ Kukoroshop/js/auth.js
@@ -1,8 +1,7 @@
 // <- CORREOS QUE TIENEN PERMISOS DE ADMIN
 export const ADMIN_EMAILS = [
-  "fmarina@unicartagena.edu.co",
   "tuadmin2@gmail.com",
-  "tuadmin3@gmail.com",
-  "soporte@wyvernstore.com"
+  "tuadmin3@gmail.com"
 ];
+
 
