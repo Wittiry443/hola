1865ff56d6ca611992e06d997b598a3405db0b9b
Wittiry443 <ecanasj@unicartagena.edu.co>
2025-12-10 11:20:00 -0500

    Update config.js

--- Kukoroshop/js/config.js
+++ Kukoroshop/js/config.js
@@ -1,4 +1,3 @@
-// js/config.js
-export const API_URL = "https://a.thepersonmrt.workers.dev/"; // Ajusta si tu worker cambia
+export const API_URL = "https://a.thepersonmrt.workers.dev/";
 export const WHATSAPP_NUMBER = "573207378992";
 export const CART_STORAGE_KEY = "shop_cart_v1";
